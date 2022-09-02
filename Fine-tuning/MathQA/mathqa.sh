d6308e4f00c9e7d16f225cd272724afaf5bcaeda
刘世伟 <liushiwei@v095217.vpn.tue.nl>
2022-09-01 23:08:02 -0500

    add path of pretrained model

--- Fine-tuning/MathQA/mathqa.sh
+++ Fine-tuning/MathQA/mathqa.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+#SBATCH --job-name=MWP-BERT-mathqa-dense
+#SBATCH -p gpu
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --gpus=1
+#SBATCH -t 1-00:00:00
+#SBATCH --exclusive
+#SBATCH --cpus-per-task=18
+#SBATCH -o MWP-BERT-mathqa-dense.out
+
+source /home/sliu/miniconda3/etc/profile.d/conda.sh
+source activate slak
 
 python run_ft.py \
-    --output_dir output/bert-base-uncased \
+    --output_dir /home/sliu/project_space/pruning_fails/QA/mathQA/bert_dense/ \
     --bert_pretrain_path /home/sliu/project_space/pruning_fails/QA/mathQA/pretrained_models/MWP-BERT_en \
     --data_dir data \
     --train_file MathQA_bert_token_train.json \
@@ -9,9 +22,9 @@ python run_ft.py \
     --dev_file MathQA_bert_token_val.json \
     --test_file MathQA_bert_token_test.json \
     --schedule linear \
-    --batch_size 8 \
+    --batch_size 32 \
     --learning_rate 0.0001 \
-    --n_epochs 100 \
+    --n_epochs 80 \
     --warmup_steps 4000 \
     --n_save_ckpt 3 \
     --n_val 5 \
