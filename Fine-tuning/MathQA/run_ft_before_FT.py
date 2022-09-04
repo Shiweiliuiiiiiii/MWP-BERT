cc11f3741a037647cdfe12b81e90af5e05d29666
刘世伟 <liushiwei@v095172.vpn.tue.nl>
2022-09-03 22:20:23 -0500

    fix modules issue

--- Fine-tuning/MathQA/run_ft_before_FT.py
+++ Fine-tuning/MathQA/run_ft_before_FT.py
@@ -214,6 +214,7 @@ def train_model(args, train_pairs, test_pairs, generate_num_ids,
             torch.nn.utils.clip_grad_norm_(need_optimized_parameters, args.max_grad_norm)
 
             optimizer.step()
+            if mask: mask.step()
             scheduler.step()
 
             encoder.zero_grad()
@@ -231,7 +232,7 @@ def train_model(args, train_pairs, test_pairs, generate_num_ids,
         logger.info("--------------------------------")
         del input_batches, input_lengths, output_batches, output_lengths, nums_batches, num_stack_batches, num_pos_batches, num_size_batches
 
-        if epoch % args.n_val == 0 or epoch > args.n_epochs - 5:
+        if epoch % args.n_val == 0 and epoch > args.n_epochs - 5:
             value_ac = 0
             equation_ac = 0
             eval_total = 0
